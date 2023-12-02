25f0877c060c5bc96549b84e943bfd3a071456a9
paulpatterson <paul@pattersonmultimedia.com>
2023-12-02 13:27:57 -0600

    Fixed routing issue with WarehouseDrivers

--- api/mergedData.js
+++ api/mergedData.js
@@ -13,17 +13,20 @@ const viewDriverDetails = (driverFirebaseKey) => new Promise((resolve, reject) =
 });
 
 // This function helps us see details about the warehouse. it takes a special code for a warehouse, finds that warehouse's information and also looks for all the drivers in the warehouse. Then it puts all this information together and gives it back.
-const viewWarehouseDetails = async (warehouseFirebaseKey) => {
-  const warehouse = await getSingleWarehouse(warehouseFirebaseKey);
-  const drivers = await getDriverWarehouse(warehouse.firebaseKey);
-  return { ...warehouse, drivers };
-};
+const viewWarehouseDetails = (warehouseFirebaseKey) => new Promise((resolve, reject) => {
+  Promise.all([getSingleDriver(warehouseFirebaseKey), getDriverWarehouse(warehouseFirebaseKey)])
+    .then(([warehouseObject, warehouseDriverArray]) => {
+      console.warn(warehouseDriverArray);
+      resolve({ ...warehouseObject, drivers: warehouseDriverArray });
+    }).catch((error) => reject(error));
+});
 
 // This function is for when we want to remove a whole warehouse and all it's drivers. It first finds all the drivers in that warehouse, then goes through each driver and removes them. After that, it deletes the warehouse itself. It's like cleaning up everythnig related to that warehouse.
 const deleteWarehouseDrivers = (firebaseKey) => new Promise((resolve, reject) => {
   getDriverWarehouse(firebaseKey).then((warehouseDriverArray) => {
+    console.warn(`The value of driver warehouse is ${warehouseDriverArray}`);
     const deleteDriverPromises = warehouseDriverArray.map((driver) => deleteDriver(driver.firebaseKey));
-
+    console.warn(warehouseDriverArray);
     Promise.all(deleteDriverPromises).then(() => {
       deleteSingleWarehouse(firebaseKey).then(resolve);
     });
